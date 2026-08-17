d6bb5918732d73f08664112b5dac5ee2dd563026
shayan1111 <69235308+shayan1111@users.noreply.github.com>
2026-08-17 17:24:55 +0330

    Delete rate-repository-api directory

--- rate-repository-api/src/api/repositories/index.js
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-
-import getRepositories from './getRepositories.js';
-
-const router = new Router();
-
-router.get('/', getRepositories);
-
-export default router;
--- rate-repository-api/src/utils/verifyJwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-import { JWT_SECRET } from '../config.js';
-
-const verifyJwt = (token, options) => {
-  return jwt.verify(token, JWT_SECRET, options);
-};
-
-export default verifyJwt;
